c537f103ee2fb999d9559b898bc9741273b75b3f
l4ndl0s1 <32853958+l4ndl0s1@users.noreply.github.com>
2024-05-19 11:20:34 +0200

    final?

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "c366341fa369cb9415a174a884997d56",
-"index.html": "5981438b23344e36bb2cca5ec9fe1cde",
-"/": "5981438b23344e36bb2cca5ec9fe1cde",
-"main.dart.js": "22631fc87d9a971a7c01fdfe8e2e55d8",
+"index.html": "0560497d02466e4f355f5cc23b56ed01",
+"/": "0560497d02466e4f355f5cc23b56ed01",
+"main.dart.js": "befc3fd6c171bf554d7749b085876079",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
